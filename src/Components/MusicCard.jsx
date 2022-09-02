d9cfa51e99148d0bb255ba0e736c5a255d6a52d9
Matheus Massara <matheus.massara@hotmail.com>
2022-09-02 17:34:53 -0300

    Restaurando arquivo.

--- src/Components/MusicCard.jsx
+++ src/Components/MusicCard.jsx
@@ -1,41 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import { getFavoriteSongs } from "../services/favoriteSongsAPI";
+
 
 class MusicCard extends React.Component {
-  render() {
-    const { previewUrl, musicName, trackId, onChange, favorite } = this.props;
 
+  render() {
+    const { previewUrl, musicName, trackId, onChange, favorite} = this.props;
+    
     return (
       <li>
         <section>{musicName}</section>
-        <audio data-testid="audio-component" src={ previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          {' '}
-          <code>audio</code>
-          .
+        <audio data-testid="audio-component" src={previewUrl} controls>
+          <track kind="captions" />O seu navegador não suporta o elemento{" "}
+          <code>audio</code>.
         </audio>
-        <label data-testid={ `checkbox-music-${trackId}` } htmlFor="favorita">
-          Favorita
-          <input
-            type="checkbox"
-            onChange={ onChange }
-            data-trackid={ trackId }
-            checked={ favorite }
-            id="favorita"
-          />
+        <label data-testid={`checkbox-music-${trackId}`}>Favorita
+        <input type='checkbox' onChange={onChange} data-trackid={trackId}
+        checked={favorite}></input>
         </label>
       </li>
     );
   }
 }
-
-// MusicCard.propTypes = {
-//   previewUrl: PropTypes.string.isRequired,
-//   musicName: PropTypes.string.isRequired,
-//   trackId: PropTypes.number.isRequired,
-//   onChange: PropTypes.func.isRequired,
-//   favorite: PropTypes.bool.isRequired,
-// };
 
 export default MusicCard;
